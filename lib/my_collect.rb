866df81541573784a1a57177cfe1847655bf0d2e
TimmmyD <tmd112593@gmail.com>
2020-10-27 12:46:44 -0700

    commit tree 95a2da04710067efab7e92f794d2468624a8546d

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+    index = 0
+    new_array = []
+    while(index < collection.length) do
+        new_array[index] = yield(collection[index])
+        index += 1
+    end
+    new_array
+end
 
